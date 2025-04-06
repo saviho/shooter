30271970898fbf77402f720e51a1c4442bba3c00
saviho <saviho228@gmail.com>
2025-04-06 16:50:56 +0300

    Update code.py
    убраны частично комментарии

--- code.py
+++ code.py
@@ -1,4 +1,4 @@
-from pygame import *
+rom pygame import *
 from random import randint
 font.init()
 font1 = font.Font(None, 80)
@@ -39,31 +39,28 @@ class GameSprite(sprite.Sprite):
        #вызываем конструктор класса (Sprite):
         sprite.Sprite.__init__(self)
 
-
-       #каждый спрайт должен хранить свойство image - изображение
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
 
 
-       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
- #метод, отрисовывающий героя на окне
+
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-#класс главного игрока
+
 class Player(GameSprite):
-   #метод для управления спрайтом стрелками клавиатуры
+
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
             self.rect.x -= self.speed
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
- #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+
     def fire(self):
         bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
         bullets.add(bullet)
@@ -72,70 +69,68 @@ class Player(GameSprite):
 
 
 
-#класс спрайта-врага  
+  
 class Enemy(GameSprite):
-   #движение врага
+
     def update(self):
         self.rect.y += self.speed
         global lost
-       #исчезает, если дойдёт до края экрана
+
         if self.rect.y > win_height:
             self.rect.x = randint(80, win_width - 80)
             self.rect.y = 0
             lost = lost + 1
-#класс спрайта-пули  
+ 
 class Bullet(GameSprite):
-   # движение врага
+
     def update(self):
         self.rect.y += self.speed
-       # исчезает, если дойдет до края экрана
+
         if self.rect.y < 0:
             self.kill()
-#создаём окошко
+
 win_width = 700
 win_height = 500
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
-#создаём спрайты
+
 ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-#создание группы спрайтов-врагов
+
 monsters = sprite.Group()
 for i in range(1, 6):
     monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
     monsters.add(monster)
 bullets = sprite.Group()
-#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
 finish = False
-#основной цикл игры:
-run = True #флаг сбрасывается кнопкой закрытия окна
+
+run = True 
 while run:
-   #событие нажатия на кнопку Закрыть
+
     for e in event.get():
         if e.type == QUIT:
            run = False
-        #событие нажатия на пробел - спрайт стреляет
+
         elif e.type == KEYDOWN:
             if e.key == K_SPACE:
                 fire_sound.play()
                 ship.fire()
- #сама игра: действия спрайтов, проверка правил игры, перерисовка
+
     if not finish:
-       #обновляем фон
+
         window.blit(background,(0,0))
 
 
-       #производим движения спрайтов
+
         ship.update()
         monsters.update()
         bullets.update()
 
 
-       #обновляем их в новом местоположении при каждой итерации цикла
         ship.reset()
         monsters.draw(window)
         bullets.draw(window)
-       #проверка столкновения пули и монстров (и монстр, и пуля при касании исчезают)
+      
         collides = sprite.groupcollide(monsters, bullets, True, True)
         for c in collides:
            #этот цикл повторится столько раз, сколько монстров подбито
@@ -144,20 +139,19 @@ while run:
             monsters.add(monster)
 
 
-       #возможный проигрыш: пропустили слишком много или герой столкнулся с врагом
         if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
             finish = True #проиграли, ставим фон и больше не управляем спрайтами.
             window.blit(lose, (200, 200))
 
 
-       #проверка выигрыша: сколько очков набрали?
+
         if score >= goal:
             finish = True
             window.blit(win, (200, 200))
 
 
 
-       #пишем текст на экране
+
         text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
         window.blit(text, (10, 20))
 
@@ -165,12 +159,8 @@ while run:
         text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
         window.blit(text_lose, (10, 50))
 
-        #text_rel = rel.render(str(n_bullet)), 1, (255, 255, 255)
-        #window.blit(text_lose, (10, 650))
-
 
         display.update()
-   #бонус: автоматический перезапуск игры
     else:
         finish = False
         score = 0
